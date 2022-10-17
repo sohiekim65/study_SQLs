733abd42b77d196793f003206f3c0a6b40f5dfaf
sohiekim65 <sohiekim65@gamil.com>
2022-10-17 14:53:02 +0900

    sohiekim

--- sohiekim/w3schools/Employees_Order_totals.sql
+++ sohiekim/w3schools/Employees_Order_totals.sql
@@ -1,7 +1,8 @@
+-- 직원(Employees) EmployeeID가 7, 9인 종업원이 받는 주문(Orders) 총 수는 ?
 SELECT * 
 FROM Orders
 WHERE EmployeeID IN(SELECT EmployeeID
-					FROM Employees
+				FROM Employees
 					WHERE EmployeeID IN(7,9));
 
         
