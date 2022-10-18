4a571abeb1d6b470d3081d7c9c8339f147970f7d
jhun1020 <jhun1020.kh@gmail.com>
2022-10-18 14:45:21 +0900

    jhun1020

--- jhun1020/w3schools/CategoryName_Products_Order.sql
+++ jhun1020/w3schools/CategoryName_Products_Order.sql
@@ -1,5 +1,5 @@
 -- + 7 : CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 주문  OrderID 내역과 총 갯수는 ? 
-SELECT DISTINCT OrderID		-- 총 개수 = 379
+SELECT  COUNT(*)		-- 총 개수 = 482
 FROM OrderDetails
 WHERE ProductID IN (SELECT ProductID
 			        FROM Products
@@ -9,9 +9,11 @@ WHERE ProductID IN (SELECT ProductID
 			        AND Price BETWEEN 10 AND 50);
 
 
-SELECT *
- FROM Products
-WHERE CategoryID IN(SELECT CategoryID
-					FROM Categories
-					 WHERE CategoryName IN ('Dairy Products', 'Seafood') )
-AND Price BETWEEN 10 AND 50;
+SELECT  OrderID		-- OrderID 내역S 
+FROM OrderDetails
+WHERE ProductID IN (SELECT ProductID
+			        FROM Products
+			        WHERE CategoryID IN(SELECT CategoryID
+							            FROM Categories
+							            WHERE CategoryName IN ('Dairy Products', 'Seafood') )
+			        AND Price BETWEEN 10 AND 50);
