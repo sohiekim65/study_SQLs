58eaaaeb63c0ffb7fe690aecfdc1644ae49c1cb4
jhun1020 <jhun1020.kh@gmail.com>
2022-10-18 15:17:19 +0900

    jhun1020

--- jhun1020/w3schools/Categories_Products_Count.sql
+++ jhun1020/w3schools/Categories_Products_Count.sql
@@ -1,4 +1,4 @@
--- Categories별로 구성된 제품 갯수 각각 표시하세요
-SELECT CategoryID, COUNT(ProductName)
+-- Categories별로 구성된 제품 갯수 가격 평균/ 최고값/ 최저값 표시하세요
+SELECT CategoryID, COUNT(ProductName), AVG(Price), MAX(Price), MIN(Price)
 FROM Products
 GROUP BY CategoryID
