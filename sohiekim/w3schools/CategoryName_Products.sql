ac278f02378f5e45ecf48e1e2135b5a3b6c60272
sohiekim65 <sohiekim65@gmail.com>
2022-10-18 13:18:29 +0900

    sohiekim

--- sohiekim/w3schools/CategoryName_Products.sql
+++ sohiekim/w3schools/CategoryName_Products.sql
@@ -1,15 +1,6 @@
--- CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 합계는 ?
--- 결과 : 390.34
-SELECT SUM(Price)
-FROM Products
-WHERE CategoryID IN(SELECT CategoryID
-				FROM Categories
-       			WHERE CategoryName IN('Dairy Products', 'Seafood'))
-AND Price BETWEEN 10 AND 50;
-
--- CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 평균은 ?
--- 결과 : 24.396250
-SELECT AVG(Price)
+-- CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 합계, 평균은 ?
+-- 합계 결과 : 390.34   평균 결과 : 24.396250
+SELECT SUM(Price), AVG(Price)
 FROM Products
 WHERE CategoryID IN(SELECT CategoryID
 				FROM Categories
