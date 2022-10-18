90d876210e287fb8a924182226feae30e26a91eb
sohiekim65 <sohiekim65@gmail.com>
2022-10-18 15:23:51 +0900

    sohiekim

--- sohiekim/w3schools/Categories_Products_Count.sql
+++ sohiekim/w3schools/Categories_Products_Count.sql
@@ -1,4 +1,4 @@
 -- Categories별로 구성된 제품 갯수, 가격 평균/최고값/최저값은?
-SELECT COUNT(*), AVG(Price), MAX(Price), MIN(Price)
+SELECT CategoryID, COUNT(*), AVG(Price), MAX(Price), MIN(Price)
 FROM Products
 GROUP BY CategoryID;
