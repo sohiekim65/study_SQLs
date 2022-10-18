07f2ca3a2ad8f7e8eaf73690456d04845a52fb87
sohiekim65 <sohiekim65@gmail.com>
2022-10-18 13:37:05 +0900

    sohiekim

--- sohiekim/w3schools/CategoryName_Products_Order.sql
+++ sohiekim/w3schools/CategoryName_Products_Order.sql
@@ -1,10 +1,12 @@
 -- CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 주문  OrderID 내역과 총 갯수는 ?
--- 결과 : 482
+-- 결과 : 379
 SELECT OrderID
-FROM OrderDetails
-WHERE ProductID IN(SELECT ProductID
-					FROM Products
-					WHERE CategoryID IN(SELECT CategoryID
-									FROM Categories
-       								WHERE CategoryName IN('Dairy Products', 'Seafood'))
-					AND Price BETWEEN 10 AND 50);
+FROM Orders
+WHERE OrderID IN(SELECT OrderID 
+				FROM OrderDetails
+                WHERE ProductID IN(SELECT ProductID
+									FROM Products
+									WHERE CategoryID IN(SELECT CategoryID
+														FROM Categories
+       													WHERE CategoryName IN('Dairy Products', 'Seafood'))
+									AND Price BETWEEN 10 AND 50));
