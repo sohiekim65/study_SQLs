8d88f35010070cc4286dc4b2a509a385a34dcab3
jhun1020 <jhun1020.kh@gmail.com>
2022-10-18 13:23:31 +0900

    jhun1020

--- jhun1020/w3schools/CategoryName_Products_Order.sql
+++ jhun1020/w3schools/CategoryName_Products_Order.sql
@@ -6,4 +6,4 @@ WHERE ProductID IN (SELECT ProductID
 			        WHERE CategoryID IN(SELECT CategoryID
 							            FROM Categories
 							            WHERE CategoryName IN ('Dairy Products', 'Seafood') )
-			        AND Price BETWEEN 10 AND 50)
+			        AND Price BETWEEN 10 AND 50);
