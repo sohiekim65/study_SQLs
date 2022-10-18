e9b1150454b24cfb757cfb304f014e735d295cdf
jhun1020 <jhun1020.kh@gmail.com>
2022-10-18 12:46:45 +0900

    jhun1020

--- jhun1020/w3schools/CategoryName_Products.sql
+++ jhun1020/w3schools/CategoryName_Products.sql
@@ -1,6 +1,7 @@
 -- + 6 : CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 합계와 평균은 ?
 
 SELECT SUM(Price)  --  합계 = 390.34 
+FROM Products
 WHERE CategoryID IN(SELECT CategoryID
 			    FROM Categories
 			    WHERE CategoryName IN ('Dairy Products', 'Seafood') )
