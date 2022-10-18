c256cac9fba09c53115586577d9beb4f9c8a0b85
jhun1020 <jhun1020.kh@gmail.com>
2022-10-18 13:22:35 +0900

    jhun1020

--- jhun1020/w3schools/CategoryName_Products.sql
+++ jhun1020/w3schools/CategoryName_Products.sql
@@ -1,15 +1,8 @@
 -- + 6 : CategoryName 가 Dairy Products, Seafood이고 Price 10.00이상 50.00이하인 합계와 평균은 ?
 
-SELECT SUM(Price)  --  합계 = 390.34 
+SELECT SUM(Price), AVG(Price)  --  합계 = 390.34 /  평균 = 24.396250
 FROM Products
 WHERE CategoryID IN(SELECT CategoryID
 			     FROM Categories
 			     WHERE CategoryName IN ('Dairy Products', 'Seafood') )
-AND Price BETWEEN 10 AND 50;
-
-SELECT AVG(Price)  --  평균 = 24.396250
-FROM Products
-WHERE CategoryID IN(SELECT CategoryID
-			        FROM Categories
-			        WHERE CategoryName IN ('Dairy Products', 'Seafood') )
 AND Price BETWEEN 10 AND 50;
