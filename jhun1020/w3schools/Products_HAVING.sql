fad43a1237b82e370351087caf49bd66eae7e654
jhun1020 <jhun1020.kh@gmail.com>
2022-10-19 10:17:46 +0900

    jhun1020_HAVING

--- jhun1020/w3schools/Products_HAVING.sql
+++ jhun1020/w3schools/Products_HAVING.sql
@@ -1,6 +1,5 @@
 -- +1 : Products가 카테고리 별로 10 이상인 제품이 무엇인지 확인
-
-SELECT COUNT(*), CategoryID
+SELECT COUNT(ProductName), CategoryID
 FROM Products
-GROUP BY CategoryID
-HAVING COUNT(*)>=10
+GROUP BY (CategoryID)
+HAVING COUNT(ProductName) >=10
