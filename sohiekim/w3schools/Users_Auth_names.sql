ffb7c3c91a565f581c018ea242d2cfc6a4c936e0
sohiekim65 <sohiekim65@gmail.com>
2022-10-20 15:01:19 +0900

    sohiekim

--- sohiekim/w3schools/Users_Auth_names.sql
+++ sohiekim/w3schools/Users_Auth_names.sql
@@ -5,5 +5,4 @@ FROM (users INNER JOIN auths
 			ON users.UNIQUE_ID = auths.UNIQUE_ID_USERS
             AND users.NAME IN('Teddy', 'Allen')) INNER JOIN auth_names
 														ON auths.UNIQUE_ID_AUTH_NAMES = auth_names.UNIQUE_ID
-
 ;
