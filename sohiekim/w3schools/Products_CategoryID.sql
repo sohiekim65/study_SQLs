a55b573f37d93cd6fd2430acb3593619a4261e92
sohiekim65 <sohiekim65@gmail.com>
2022-10-20 12:54:04 +0900

    sohiekim

--- sohiekim/w3schools/Products_CategoryID.sql
+++ sohiekim/w3schools/Products_CategoryID.sql
@@ -1,6 +1,6 @@
 -- Products에 CategoryID가 5,6를 표시, 내용?
 SELECT Products.ProductName, Supplier.SupplierName
-FROM Products INNER JOIN Supplier
-ON Products.SupplierID = Supplier.SupplierID
+FROM Products INNER JOIN Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
 AND Products.CategoryID IN (5,6)
 ;    
