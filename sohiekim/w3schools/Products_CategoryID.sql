5713d8ed1186f7fd05737ecf06361259fcbca301
sohiekim65 <sohiekim65@gmail.com>
2022-10-20 13:21:54 +0900

    sohiekim

--- sohiekim/w3schools/Products_CategoryID.sql
+++ sohiekim/w3schools/Products_CategoryID.sql
@@ -1,4 +1,5 @@
--- Products에 CategoryID가 5,6를 표시, 내용?
+-- Products에 CategoryID가 5,6를 표시, 내용 프로턱트 이름, 공급자 이름 출력?
+-- 결과 : 13개
 SELECT Products.ProductName, Suppliers.SupplierName
 FROM Products INNER JOIN Suppliers
 ON Products.SupplierID = Suppliers.SupplierID
